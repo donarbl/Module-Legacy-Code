ed9716e89fae545c8cd850a7e8fbb8345ab281a3
donarbl <donarablanc@gmail.com>
2026-02-27 20:20:01 +0000

    fix: enforce 280 character limit on blooms in schema and API

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE users (
 CREATE TABLE blooms (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     sender_id INT NOT NULL REFERENCES users(id),
-    content TEXT NOT NULL,
+    content VARCHAR(280) NOT NULL,
     send_timestamp TIMESTAMP NOT NULL
 );
 
